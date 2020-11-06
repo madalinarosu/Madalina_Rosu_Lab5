2e8d5163ed828b957e07ba7162c863621866fc6c
Rosu Madalina <mada.rosu99@yahoo.com>
2020-11-06 14:16:16 +0200

    Add project files.

---
+++ PhoneNumbersDataSetTableAdapters/PhoneNumbersTableAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Madalina_Rosu_Lab5.PhoneNumbersDataSetTableAdapters
+{
+    internal class PhoneNumbersTableAdapter
+    {
+        internal void Fill(object phoneNumbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Update(object phoneNumbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
